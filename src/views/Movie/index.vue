e595ff5cf849ef5dcd81893f14ee1a074337f6af
songtxiang <1875430433@qq.com>
2019-05-21 19:33:22 +0800

    cs2

---
+++ src/views/Movie/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="div1">
+        Hello Movie
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .div1{
+        color: red;
+        font-size: 24px;
+    }
+</style>
